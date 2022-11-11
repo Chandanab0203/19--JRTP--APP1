3c4c8e805b63ad993f22ec734b193fdea4f19662
Chandana <chandanab980302@gmail.com>
2022-11-11 13:46:59 +0530

    changed pwdutils class

--- src/main/java/in/ashokit/PasswordUtils.java
+++ src/main/java/in/ashokit/PasswordUtils.java
@@ -4,17 +4,11 @@ public class PasswordUtils {
 
 	public void decode ()
 	{
-		//logic here
-		
-		//bug fix 104 
-		//bug 104 practice
-		System.out.println("Bug 104 fixed");
+		System.out.println("HELLO");
 	}
 	public void encode ()
 	{
-		//changes to bug 101
-		//shggsdb
-		///Bug 101 practice
+		System.out.println("HELLO");
 	}
 }
 
